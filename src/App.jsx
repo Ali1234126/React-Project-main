755882983c966e0ca743bc7041b42eefe6685e60
Ali1234126 <alihessein551@gmail.com>
2025-02-08 17:32:00 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Version 2</h1>
+      <h1>Ali hussein</h1>
+      <h1>Ali2hussein</h1>
+    </>
+  )
+}
+
+export default App
